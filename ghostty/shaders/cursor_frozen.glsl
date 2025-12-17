18df90cc30e7c7ddc37d6b5591caec811ec9691a
windyd <windyd2@sina.com>
2025-12-17 11:08:31 +0800

    [ghostty] quick terminal size update and cursor shader update

--- ghostty/shaders/cursor_frozen.glsl
+++ ghostty/shaders/cursor_frozen.glsl
@@ -71,7 +71,7 @@ float ease(float x) {
 
 const vec4 TRAIL_COLOR = vec4(.502, 0.98, 1., 1.0);
 const vec4 TRAIL_COLOR_ACCENT = vec4(.0, 0., 1., 1.0);
-const float DURATION = 0.3; //IN SECONDS
+const float DURATION = 0.1; //IN SECONDS; 0.3 by default
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
